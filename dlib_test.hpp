40cb137209429d3afb51a903816b2bbca1647c3a
Juraj Matuš <matus.juraj@yandex.com>
2017-04-13 11:14:21 +0200

    Dlib test next attempt

--- dlib_test.hpp
+++ dlib_test.hpp
@@ -2,7 +2,7 @@
 #define DLIB_TEST_HPP_
 
 #define DLIB_NO_GUI_SUPPORT
-#define NO_MAKEFILE
+//#define NO_MAKEFILE
 
 #include <dlib/image_processing/frontal_face_detector.h>
 #include <dlib/image_processing.h>
